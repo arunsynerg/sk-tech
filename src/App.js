1ada175f4b03ca06b4c7a5bee02fb235a3521456
ArunKumar <arun@ESTPL.ESSELSYNERG.COM>
2024-03-05 17:48:09 +0530

    screen overwhelming fixed

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Backgorund />
-      <Home />
+       <Backgorund />
+     <Home />
       <About />
        <Services />
       <Contact />
